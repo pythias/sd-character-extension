dd18ad7c0595a241ee0d64e48145bfe5606be950
pythias <pythias@gmail.com>
2023-04-19 16:37:00 +0800

    args

--- preload.py
+++ preload.py
@@ -4,6 +4,6 @@ import argparse
 def preload(parser: argparse.ArgumentParser):
     parser.add_argument("--character-server-name", type=str, default="Character", help="Character service name")
     parser.add_argument("--character-ignore-signature", action='store_true', help="Ignore signature check")
-    parser.add_argument("--character-api-only", action='store_true', default=True, help="Only use character api")
-    parser.add_argument("--character-nsfw-filter", action='store_true', default=True, help="Filter nsfw images")
+    parser.add_argument("--character-api-only", action='store_true', help="Only use character api")
+    parser.add_argument("--character-nsfw-filter", action='store_true', help="Filter nsfw images")
     
