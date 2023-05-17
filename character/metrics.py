fbd4b75b03e82618f570804c451777e6caad8a9e
pythias <pythias@gmail.com>
2023-05-17 10:32:54 +0800

    info once

--- character/metrics.py
+++ character/metrics.py
@@ -2,25 +2,32 @@ from modules import shared
 from prometheus_client import Info, Histogram, Counter, Gauge
 from pynvml import *
 
-nvmlInit()
+iCharacter = None
 
-totalMemory = 0
-gpuCount = nvmlDeviceGetCount()
-for i in range(gpuCount):
-    handle = nvmlDeviceGetHandleByIndex(i)
-    totalMemory += nvmlDeviceGetMemoryInfo(handle).total
+def metrics_init():
+    if iCharacter != None:
+        return
 
-i = Info('sd_character', 'Description of sd-character-extension')
-i.info({
-    'version': '1.0.4',
-    'name': shared.cmd_opts.character_server_name,
-    'driver': nvmlSystemGetDriverVersion(),
-    'total_gpu_memory': f"{totalMemory}",
-})
+    nvmlInit()
 
-nvmlShutdown()
+    totalMemory = 0
+    gpuCount = nvmlDeviceGetCount()
+    for i in range(gpuCount):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        totalMemory += nvmlDeviceGetMemoryInfo(handle).total
 
-# (.005, .01, .025, .05, .075, .1, .25, .5, .75, 1.0, 2.5, 5.0, 7.5, 10.0, INF)
+    iCharacter = Info('sd_character', 'Description of sd-character-extension')
+    iCharacter.info({
+        'version': '1.0.4',
+        'name': shared.cmd_opts.character_server_name,
+        'driver': nvmlSystemGetDriverVersion(),
+        'total_gpu_memory': f"{totalMemory}",
+    })
+
+    nvmlShutdown()
+
+metrics_init()
+
 hT2I = Histogram('character_t2i_latency_seconds', 'Text to image latency', buckets=(3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 12.0, 20.0, INF))
 hSD = Histogram('character_processing_latency_seconds', 'Stable diffusion processing latency', buckets=(3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 12.0, 20.0, INF))
 hDF = Histogram('character_face_latency_seconds', 'Detect face latency')
@@ -76,3 +83,4 @@ def gpu_temperature():
 gGPUUsedMemory.set_function(gpu_used_memory)
 gGPUTemperature.set_function(gpu_temperature)
 gGPUMemoryPercent.set_function(gpu_used_memory_percent)
+
