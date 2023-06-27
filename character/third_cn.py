0467ef2bbe41cb659c918db8f86295059baab6d7
pythias <pythias@gmail.com>
2023-06-27 17:29:36 +0800

    v1.3.5
    - ControlNet默认参数的处理

--- character/third_cn.py
+++ character/third_cn.py
@@ -30,27 +30,34 @@ def _find_closest_cn_model_name(search):
 lib.log(f"ControlNet {control_net_version} loaded, models: {len(control_net_models)}, lineart: {_find_closest_cn_model_name('lineart')}")
 
 def apply_args(request):
+    # 因为有些场景需要做些参数调整，简化使用方的参数传递，所以封装一下
     units = []
-    units.append(_get_cn_image_unit(request, 0, "lineart_realistic", "lineart"))
-    for i in range(1, 5):
+    for i in range(0, 5):
         units.append(_get_cn_image_unit(request, i))
 
     requests.update_script_args(request, "ControlNet", [_to_process_unit(unit) for unit in units])
 
-def _get_cn_image_unit(request, i, default_preprocessor = None, default_model = None):
+def _get_cn_image_unit(request, i):
+    image_b64 = requests.get_extra_value(request, f"image_cn_{i}", None)
+    if not image_b64:
+        # 当传递了图像参数时，默认使用lineart处理
+        default_model = "lineart"
+        default_preprocessor = "lineart_realistic"
+    else:
+        default_model = None
+        default_preprocessor = None
+
     model = requests.get_extra_value(request, f"model_cn_{i}", default_model)
     model = _find_closest_cn_model_name(model)
     if not model:
+        # 如果找不到处理的模型，则不处理
         return _get_cn_disabled_unit()
 
-    image_b64 = requests.get_extra_value(request, f"image_cn_{i}", None)
-
+    # 核心参数处理
     unit = _get_cn_empty_unit()
     unit["module"] = requests.get_extra_value(request, f"processor_cn_{i}", default_preprocessor)
     unit["model"] = model
     unit["image"] = image_b64
-
-    # todo 特殊场景的处理，比如inpainting的mask
 
     # 参数: processor_res, 以传递的参数为主, 但是如果没有传递参数, 则使用默认值/或者图像的尺寸
     processor_res = requests.get_extra_value(request, f"processor_res_cn_{i}", None)
@@ -62,6 +69,7 @@ def _get_cn_image_unit(request, i, default_preprocessor = None, default_model = 
     # 其他参数
     _fill_unit_with_extra(unit, request, i)
 
+    # todo 特殊场景的处理，比如inpainting的mask
     return unit
 
 
